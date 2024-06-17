9779ac47dcd3ae39c5098f27d34158e7c91702fd
02xmysummer <129921953+02xmysummer@users.noreply.github.com>
2024-06-17 11:07:42 +0800

    Add files via upload

--- kof/connections.h
+++ kof/connections.h
@@ -3,21 +3,33 @@
 
 #include <QObject>
 #include <QUdpSocket>
+enum MessageType{Message,NewParticipant,ParticipantLeft};
 class Connections : public QObject
 {
     Q_OBJECT
 public:
     explicit Connections(QObject *parent = nullptr);
 
-    QString getLocalIp();
-    void onSocketReadyRead();
+protected:
+    void newParticipant(QString userName,QString localHostName,QString ipAddress);
+    void participantLeft(QString userName,QString localHostName);
+    void sentMessage(MessageType type,QString serverAdress = "");
+    QString getIp();
+    QString getUserName();
+    QString getMessage();
+
+
 signals:
 
 private:
     QUdpSocket *m_udpSocket;
+    qint16 m_port;
     QString m_targetIp;
-    int m_targetPort = 4000;
+    qint16 m_targetPort;
     QString m_targetMessage;
+
+
+    void processPendingDatagrams();
 };
 
 #endif // CONNECTIONS_H
